4d3e16e800132fafc18ba2e5f232402deec4ef0d
Abraham-Thomas <15197498389@163.com>
2021-06-24 21:35:27 +0800

    finish the senior-surface of  the first project

---
+++ Project/Armed spaceship/alien_invasion.py
@@ -0,0 +1,51 @@
+
+# 模块pygame包含开发游戏所需的功能。玩家退出时，使用模块sys中的工具来退出。
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """ 管理游戏资源和行为的类 """
+
+    def __init__(self):
+        """ 初始化游戏并创建游戏资源 """
+        pygame.init()
+        self.settings = Settings()
+
+        # 设置背景色(浅灰色)
+        self.bg_color = (230, 230, 230)
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """ 开始游戏的主循环 """
+        while True:
+            # 监视键盘和鼠标事件
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+
+            # 每次循环时都重绘屏幕
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # 让最近绘制的屏幕可见。
+            pygame.display.flip()
+
+
+
+
+if __name__ == '__main__':
+    # 创建游戏实例并运行游戏。
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+
+

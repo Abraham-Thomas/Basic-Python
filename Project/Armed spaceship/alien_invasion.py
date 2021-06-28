f2986236d779b5669fd8d61a945628d925285106
Abraham-Thomas <15197498389@163.com>
2021-06-28 17:14:39 +0800

    set quit and fullscreen

--- Project/Armed spaceship/alien_invasion.py
+++ Project/Armed spaceship/alien_invasion.py
@@ -17,7 +17,10 @@ class AlienInvasion:
         # 设置背景色(浅灰色)
         self.bg_color = (230, 230, 230)
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # 由于无法预先知道屏幕的宽度和高度，要在创建屏幕后更新这些设置
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -47,6 +50,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """ 响应松开。 """

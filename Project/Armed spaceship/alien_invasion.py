2ced3fc3631bbe8d76248029d638cd7287bc07e1
Abraham-Thomas <15197498389@163.com>
2021-06-28 18:03:56 +0800

    set shoting

--- Project/Armed spaceship/alien_invasion.py
+++ Project/Armed spaceship/alien_invasion.py
@@ -5,6 +5,8 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+
 
 class AlienInvasion:
     """ 管理游戏资源和行为的类 """
@@ -24,13 +26,30 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
 
     def run_game(self):
         """ 开始游戏的主循环 """
         while True:
             self._check_events()
             self.ship.update()
-            self._update_screen()                          
+            self.bullets.update()
+            self.__update_bullets()
+            self._update_screen()
+
+    def __update_bullets(self):
+        """ 更新子弹的位置并删除消失的子弹。"""
+        # 更新子弹的位置
+        self.bullets.update()
+
+        # 删除消失的子弹
+        # 使用for循环便利列表（或Pygame编组）时，Python要求该列表的长度在整个循环中保持不变。
+        # 因为不能从for循环遍历的列表或编组中删除元素，所以必须遍历组的副本。
+        # 使用copy()来设置for循环，从而能够在循环中修改bullets
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
 
     def _check_events(self):
@@ -52,6 +71,10 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        # 按空格键时调用_fire_bullet()。
+        elif event.key ==pygame.K_SPACE:
+            self._fire_bullet()
+
 
     def _check_keyup_events(self, event):
         """ 响应松开。 """
@@ -60,13 +83,22 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
             
-    
+    def _fire_bullet(self):
+        """ 创建一颗子弹，并将其加入编组bullets中。"""
+
+        # 在创建新子弹前检查未消失的子弹数是否小于该设置
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
 
     def _update_screen(self):
         """ 更新屏幕上的图像，并切换到新屏幕。"""
         # 每次循环时都重绘屏幕
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # 让最近绘制的屏幕可见。
         pygame.display.flip()

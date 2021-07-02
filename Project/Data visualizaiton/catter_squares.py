cc3ad9067a5a6cf359140210ee42b9b01f9bd54d
Abraham-Thomas <15197498389@163.com>
2021-07-02 21:48:34 +0800

    use matplotlib

---
+++ Project/Data visualizaiton/catter_squares.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=5)
+
+
+# 设置图标标题并给坐标轴加上标签
+
+ax.set_title("平方数", fontsize=24)
+ax.set_xlabel("值", fontsize=14)
+ax.set_ylabel("值的平方", fontsize=14)
+
+# 设置刻度标记的大小
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
+
+# 自动保存
+# plt.savefig('squares_plot.png', bbox_inches='tight')
---
+++ Project/Data visualizaiton/mpl_squares.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth=3)
+
+# 设置图标标题并给坐标轴加上标签
+
+ax.set_title("平方数", fontsize=24)
+ax.set_xlabel("值", fontsize=14)
+ax.set_ylabel("值的平方", fontsize=14)
+
+# 设置刻度标记的大小
+ax.tick_params(axis='both', labelsize=14)
+
+plt.show()
